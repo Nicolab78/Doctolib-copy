f990f855b067af88993a4eb19a04ecf905ac6b06
Nicolas <nicobou60@gmail.com>
2026-01-17 22:20:24 +0100

    appointement crud

--- doctolib-backend/src/app.ts
+++ doctolib-backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import userRoutes from "./routes/user/user.routes"
+import appointementRoutes from "./routes/appointement/appointement.routes"
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/users", userRoutes)
+app.use("/appointements", appointementRoutes)
 
 export default app
